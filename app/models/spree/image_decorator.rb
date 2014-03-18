e8a527e7f2e298b1e0e527dbaa542464510aa512
peter <peter@hikerlink.org>
2014-03-18 23:29:42 +0800

    trailing >

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -2,7 +2,7 @@ Spree::Image.class_eval do
   attachment_definitions[:attachment][:styles] = {
       :mini => '48x48>', # thumbs under image
       :small => '100x100>', # images on category view
-      :plano => '144x144',
+      :plano => '144x144>',
       :product => '240x240>', # full product image
       :large => '600x600>' # light box image
   }
